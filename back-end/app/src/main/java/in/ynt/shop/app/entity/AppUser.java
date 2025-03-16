c9d20a6fa4bcddc8c211778a713ef67b0579152a
Yuvaraj <nyuvaraj24@gmail.com>
2025-03-16 11:16:13 +0530

    added open ai configuration for testing

--- back-end/app/src/main/java/in/ynt/shop/app/entity/AppUser.java
+++ back-end/app/src/main/java/in/ynt/shop/app/entity/AppUser.java
@@ -6,6 +6,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.io.Serializable;
 import java.util.Date;
@@ -22,10 +24,24 @@ public class AppUser implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    private String fname;
-    private String lname;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
     private String email;
+
+    @Column(name = "password")
     private String password;
+
+    @CreationTimestamp
+    @Column(name = "created")
     private Date created;
+
+    @UpdateTimestamp
+    @Column(name = "updated")
     private Date updated;
 }
