89988cb6a9b78bcc698f430b16ed45881f3150d6
Yuvaraj <nyuvaraj24@gmail.com>
2025-04-06 22:09:50 +0530

    backend - added log for controller

--- back-end/app/src/main/java/in/ynt/shop/app/component/LoggingAspect.java
+++ back-end/app/src/main/java/in/ynt/shop/app/component/LoggingAspect.java
@@ -16,13 +16,25 @@ public class LoggingAspect {
 
     @Before("execution(* in.ynt.shop.app.serviceImpl.*.*(..))")
     public void logBeforeMethod(JoinPoint joinPoint) {
-        logger.info("Entering into method :: {} from class - {}", joinPoint.getSignature().getName(),
+        logger.info("Entering into method :: {} from service class - {}", joinPoint.getSignature().getName(),
                 joinPoint.getTarget().getClass().getName());
     }
 
     @After("execution(* in.ynt.shop.app.serviceImpl.*.*(..))")
     public void logAfterMethod(JoinPoint joinPoint) {
-        logger.info("Exiting from method :: {} from class - {}", joinPoint.getSignature().getName(),
+        logger.info("Exiting from method :: {} from service class - {}", joinPoint.getSignature().getName(),
+                joinPoint.getTarget().getClass().getName());
+    }
+
+    @Before("execution(* in.ynt.shop.app.controller.*.*(..))")
+    public void logBeforeControllerMethod(JoinPoint joinPoint) {
+        logger.info("Entering into method :: {} from controller class - {}", joinPoint.getSignature().getName(),
+                joinPoint.getTarget().getClass().getName());
+    }
+
+    @After("execution(* in.ynt.shop.app.serviceImpl.*.*(..))")
+    public void logAfterControllerMethod(JoinPoint joinPoint) {
+        logger.info("Exiting from method :: {} from controller class - {}", joinPoint.getSignature().getName(),
                 joinPoint.getTarget().getClass().getName());
     }
 }
